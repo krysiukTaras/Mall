765088a5b04026f3e4f3e3fdb7583e4fc64ddc85
Taras_Krysiuk <taras140991@gmail.com>
2014-12-21 00:58:53 +0200

    home

---
+++ src/main/java/com/tk/servlet/Home.java
@@ -0,0 +1,16 @@
+package com.tk.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by macbook on 21.12.14.
+ */
+public class Home extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/pages/index.jsp").forward(request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request, response);
+    }
+}
